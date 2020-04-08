853b2ccb78bb036701a82bdc0609e93a77dec70e
mjaquiery <matt.jaquiery@psy.ox.ac.uk>
2020-04-08 10:10:25 +0100

    Twitter card updates
    Updated the Twitter card to show timezone and have a cleaner form.

--- assets/js/card-edit.js
+++ assets/js/card-edit.js
@@ -4,19 +4,17 @@ function updateCard(e) {
     const card = document.getElementById('card-img');
     const form = document.getElementById('card-form');
 
-    card.querySelector('#place text').innerHTML =
-        form.querySelector('#place').value;
-    card.querySelector('#twitter text').innerHTML =
-        form.querySelector('#twitter').value;
-
-    const datetime = new Date(form.querySelector('#datetime').value);
-
-    const h = datetime.getHours();
-    const m = datetime.getMinutes();
-    const time = `${h < 10? 0 : ''}${h}:${m < 10? 0 : ''}${m}`;
-    card.querySelector('#time text.scrawl').innerHTML = time;
-
-    card.querySelector('#date text.scrawl').innerHTML = datetime.toDateString();
+    form.querySelectorAll('input, select').forEach(elm => {
+        if(!/-in$/i.test(elm.id))
+            return;
+        const re = elm.id.match(/^(\w+)-in$/i);
+        if(!re)
+            return;
+        const c = card.querySelector(`#${re[1]}`);
+        if(!c)
+            return;
+        c.innerHTML = elm.value;
+    });
 }
 
 function saveCardPNG() {
@@ -24,4 +22,19 @@ function saveCardPNG() {
     new Pablo(card).download('png', 'reproducibiliTea-card.png', (r) => console.log(r));
 }
 
+function setFieldDefaults() {
+    const d = new Date();
+    const s = d.toString().match(/GMT[+\-][0-9]{4}/);
+    if(!s)
+        return;
+    document.querySelector('#timezone-in option[value="' + s[0] + '"]')
+        .selected = "selected";
+
+    const month = d.getMonth() < 10? "0" + d.getMonth().toString() : d.getMonth().toString();
+    const day = d.getDate() < 10? "0" + d.getDate().toString() : d.getDate().toString();
+    const date = `${d.getFullYear()}-${month}-${day}`;
+    document.querySelector('#date-in').value = date;
+}
+
+setTimeout(setFieldDefaults, 150);
 setTimeout(updateCard, 200);
